efb9a08628c704ceea4e4b2d56ffb998ba729710
Eduardo Vítor <eduardovitor730@gmail.com>
2024-12-28 20:33:59 -0300

    Refat add classe parser_estados_regioes

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ from concurso_area_nlp import ConcursoAreaClassificador
 import const
 from relatorio import Relatorio
 from scraper import Scraper
+from parser_estados_regioes import ParserEstadosRegioes
 
 
 titulos_cargos = set()
 concursos_set = set()
-estados = []
-regioes = []
 
 
 def separar_links_duplicados(dados_concursos):
@@ -22,56 +21,6 @@ def separar_links_duplicados(dados_concursos):
     links_duplicados = [chave for chave,valor in contador_concurso_link.items() 
                         if valor > 1]
     return links_duplicados
-
-
-def inicializar_siglas_estados(info_estados_regioes):
-    siglas_estados = []
-    for info_estado in info_estados_regioes:
-        for chave in info_estado:
-            if chave == "sigla":
-                siglas_estados.append(info_estado["sigla"])
-    return siglas_estados
-
-
-def separar_estados_regioes_unicos(dados_concursos,siglas_estados,
-                                   info_estados_regioes,links_duplicados):
-    for registro in dados_concursos:
-        for dic in registro:
-            info_concurso_split = dic["concurso"].split("-")
-            info_concurso_split = list(map(lambda info: info.strip(),info_concurso_split))
-            combinacao_concurso_link = f"{dic['concurso']},;{dic['link']}"
-            if combinacao_concurso_link not in links_duplicados:
-                for info in info_concurso_split:
-                    if info in siglas_estados:
-                        for info_estado in info_estados_regioes:
-                            for chave in info_estado:
-                                if info_estado[chave] == info:
-                                    estados.append(info_estado["nome"])
-                                    regioes.append(info_estado["regiao"])
-
-
-def separar_estados_regioes_duplicados(links_duplicados,siglas_estados,
-                                       info_estados_regioes):
-    for link in links_duplicados:
-        info_concurso_quebra = link.split(",;")[0]
-        info_concurso_split = info_concurso_quebra.split("-")
-        info_concurso_split = list(map(lambda info: info.strip(),info_concurso_split))
-        for info in info_concurso_split:
-            if info in siglas_estados:
-                for info_estado in info_estados_regioes:
-                    for chave in info_estado:
-                        if info_estado[chave] == info:
-                            estados.append(info_estado["nome"])
-                            regioes.append(info_estado["regiao"])
-
-
-def separar_estados_regioes(dados_concursos,info_estados_regioes,links_duplicados):
-    # https://servicodados.ibge.gov.br/api/v1/localidades/estados
-    siglas_estados = inicializar_siglas_estados(info_estados_regioes)
-    separar_estados_regioes_unicos(dados_concursos,siglas_estados,
-                                   info_estados_regioes,links_duplicados)
-    separar_estados_regioes_duplicados(links_duplicados,siglas_estados,
-                                       info_estados_regioes)
 
 
 def retornar_areas_concursos(dados_concursos,links_duplicados):
@@ -139,8 +88,9 @@ if __name__ == '__main__':
     dados_concursos = ordenar_concursos(dados_concursos)
     # Separando duplicatas
     links_duplicados = separar_links_duplicados(dados_concursos)
-    # Separando estados e regiões dos concursos
-    separar_estados_regioes(dados_concursos,info_estados_regioes,links_duplicados)
+    parser_estados_regioes = ParserEstadosRegioes(info_estados_regioes,
+                                                  dados_concursos,links_duplicados)
+    estados, regioes = parser_estados_regioes.separar_estados_regioes()
     areas = retornar_areas_concursos(dados_concursos,links_duplicados)
     concursos_cargos = retornar_concursos_cargo(dados_concursos,links_duplicados)
     contador_estados = Counter(estados)
---
+++ parser_estados_regioes.py
@@ -0,0 +1,55 @@
+class ParserEstadosRegioes: # https://servicodados.ibge.gov.br/api/v1/localidades/estados
+    __estados = []
+    __regioes = []
+
+
+    def __init__(self,info_estados_regioes,dados_concursos,links_duplicados):
+        self.__info_estados_regioes = info_estados_regioes
+        self.__siglas_estados = self.inicializar_siglas_estados()
+        self.__dados_concursos = dados_concursos
+        self.__links_duplicados = links_duplicados
+
+
+    def inicializar_siglas_estados(self):
+        siglas_estados = []
+        for info_estado in self.__info_estados_regioes:
+            for chave in info_estado:
+                if chave == "sigla":
+                    siglas_estados.append(info_estado["sigla"])
+        return siglas_estados
+    
+    
+    def separar_estados_regioes_unicos(self):
+        for registro in self.__dados_concursos:
+            for dic in registro:
+                info_concurso_split = dic["concurso"].split("-")
+                info_concurso_split = list(map(lambda info: info.strip(),info_concurso_split))
+                combinacao_concurso_link = f"{dic['concurso']},;{dic['link']}"
+                if combinacao_concurso_link not in self.__links_duplicados:
+                    for info in info_concurso_split:
+                        if info in self.__siglas_estados:
+                            for info_estado in self.__info_estados_regioes:
+                                for chave in info_estado:
+                                    if info_estado[chave] == info:
+                                        self.__estados.append(info_estado["nome"])
+                                        self.__regioes.append(info_estado["regiao"])
+
+
+    def separar_estados_regioes_duplicados(self):
+        for link in self.__links_duplicados:
+            info_concurso_quebra = link.split(",;")[0]
+            info_concurso_split = info_concurso_quebra.split("-")
+            info_concurso_split = list(map(lambda info: info.strip(),info_concurso_split))
+            for info in info_concurso_split:
+                if info in self.__siglas_estados:
+                    for info_estado in self.__info_estados_regioes:
+                        for chave in info_estado:
+                            if info_estado[chave] == info:
+                                self.__estados.append(info_estado["nome"])
+                                self.__regioes.append(info_estado["regiao"])
+    
+
+    def separar_estados_regioes(self):
+        self.separar_estados_regioes_unicos()
+        self.separar_estados_regioes_duplicados()
+        return self.__estados.copy(), self.__regioes.copy()
