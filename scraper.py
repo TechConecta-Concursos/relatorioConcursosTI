62ed244c6a3b1c3d93e7bdabede7ae3b3efa7890
Eduardo Vítor <eduardovitor730@gmail.com>
2025-01-07 11:52:23 -0300

    Tratando exceções classe scraper

--- scraper.py
+++ scraper.py
@@ -7,36 +7,43 @@ class Scraper:
     
     
     def __scrapy_link(self,link,concursos_set):
-        doc_html = requests.get(link).content
-        soup_html = BeautifulSoup(doc_html, 'lxml')
-        vagas_html = soup_html.find_all("ul",{'class':['link-d', 'link-i']})
-        dics_concursos = []
-        info_concurso_skipped = False
-        info_cargo_add = False
-        dic_concurso = dict(cargo="",concurso="",link="")
-        for vaga in vagas_html:
-            if vaga["class"][0] == "link-d":
-                if vaga.li is not None:
-                    if not vaga.li.a["title"].startswith("Concursos"):
-                        dic_concurso["concurso"] = vaga.li.a.text
-                        dic_concurso["link"] = vaga.li.a["href"]
-                        info_concurso_skipped = False
-                else:
-                    info_concurso_skipped = True
-            elif vaga["class"][0] == "link-i":
-                if not info_concurso_skipped and not info_cargo_add:
-                    cargo = (vaga.li.a.text).split(" ")[0].capitalize()
-                    if "-" in cargo:
-                        cargo = cargo.split("-")[0]
-                    dic_concurso["cargo"] = cargo
-                    info_cargo_add = True
-            if info_cargo_add:
-                combinacao_concurso = f"{dic_concurso['cargo']},;{dic_concurso['concurso']},;{dic_concurso['link']}"
-                copy_concurso = dic_concurso.copy()
-                if combinacao_concurso not in concursos_set:
-                    dics_concursos.append(copy_concurso)
-                    concursos_set.add(combinacao_concurso)
+        try:
+            req = requests.get(link)
+            if req.status_code == 200:
+                doc_html = req.content
+                soup_html = BeautifulSoup(doc_html, 'lxml')
+                vagas_html = soup_html.find_all("ul",{'class':['link-d', 'link-i']})
+                dics_concursos = []
+                info_concurso_skipped = False
                 info_cargo_add = False
+                dic_concurso = dict(cargo="",concurso="",link="")
+                for vaga in vagas_html:
+                    if vaga["class"][0] == "link-d":
+                        if vaga.li is not None:
+                            if not vaga.li.a["title"].startswith("Concursos"):
+                                dic_concurso["concurso"] = vaga.li.a.text
+                                dic_concurso["link"] = vaga.li.a["href"]
+                                info_concurso_skipped = False
+                        else:
+                            info_concurso_skipped = True
+                    elif vaga["class"][0] == "link-i":
+                        if not info_concurso_skipped and not info_cargo_add:
+                            cargo = (vaga.li.a.text).split(" ")[0].capitalize()
+                            if "-" in cargo:
+                                cargo = cargo.split("-")[0]
+                            dic_concurso["cargo"] = cargo
+                            info_cargo_add = True
+                    if info_cargo_add:
+                        combinacao_concurso = f"{dic_concurso['cargo']},;{dic_concurso['concurso']},;{dic_concurso['link']}"
+                        copy_concurso = dic_concurso.copy()
+                        if combinacao_concurso not in concursos_set:
+                            dics_concursos.append(copy_concurso)
+                            concursos_set.add(combinacao_concurso)
+                        info_cargo_add = False
+            else:
+                print("Requisição ao site PCI Concursos mal-sucedida, status diferente de 200")
+        except Exception as e:
+            print(f"Erro ao abrir a conexão com o site PCI Concursos {e}")
         return dics_concursos
     
 
